39451ae8bef3e9594999a920b00f3601104afd7c
Alessandro Vaselli <ale97vas@Alessandros-MacBook-Air.local>
2026-02-12 15:11:28 +0100

    Fix deployment dependencies for Render

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ npm install --prefix client
 npm run build --prefix client
 
 echo "Installing Backend..."
+python -m pip install --upgrade pip
 pip install -r server/requirements.txt
 
 echo "Build Complete."
